7e25455280798f83dffb6ac60ca195de41ca6928
Davide Sestili <d.sestili@yahoo.it>
2017-05-29 18:44:53 +0200

    Removed unused code

--- src/it/dsestili/jhashcode/test/TestSingleFile.java
+++ src/it/dsestili/jhashcode/test/TestSingleFile.java
@@ -20,9 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import java.io.File;
 import java.text.DecimalFormat;
-
-//import java.util.Timer;
-//import java.util.TimerTask;
 import it.dsestili.jhashcode.core.Core;
 import it.dsestili.jhashcode.core.IProgressListener;
 import it.dsestili.jhashcode.core.ProgressEvent;
@@ -34,15 +31,6 @@ public class TestSingleFile implements IProgressListener
 
 	private void test(String fileName)
 	{
-		/*
-		String[] algorithms = Core.getAlgorithms();
-		
-		for(String algorithm : algorithms)
-		{
-			System.out.println(algorithm);
-		}
-		*/
-		
 		try
 		{
 			MainWindow.setItalianLocale();
@@ -62,29 +50,18 @@ public class TestSingleFile implements IProgressListener
 				return;
 			}
 
-                        if(!file.isFile())
+            if(!file.isFile())
 			{
 				System.out.println("Is not a file!");
 				return;
 			}
 			
-                        String algorithm = "SHA-224";
-                        MessageDigest.getInstance(algorithm);
-                        System.out.println("Algorithm " + algorithm + " exist!");
+            String algorithm = "SHA-224";
+            MessageDigest.getInstance(algorithm);
+            System.out.println("Algorithm " + algorithm + " exist!");
                         
-			final Core core = new Core(file, algorithm);
+			Core core = new Core(file, algorithm);
 			core.addIProgressListener(this);
-
-			/*
-			Timer timer = new Timer(true);
-			timer.schedule(new TimerTask() {
-				@Override
-				public void run() 
-				{
-					core.interrupt();
-				}
-			}, 10000);
-			*/
 
 			long start = System.currentTimeMillis();
 			String hash = core.generateHash();
@@ -108,14 +85,14 @@ public class TestSingleFile implements IProgressListener
 	
 	public static void main(String[] args) 
 	{
-            if(args.length > 0)
-            {
-                    new TestSingleFile().test(args[0]);
-            }
-            else
-            {
-                    System.out.println("Parameter missing!");
-            }
+        if(args.length > 0)
+        {
+                new TestSingleFile().test(args[0]);
+        }
+        else
+        {
+                System.out.println("Parameter missing!");
+        }
 	}
 
 	public void progressEvent(ProgressEvent event) 
