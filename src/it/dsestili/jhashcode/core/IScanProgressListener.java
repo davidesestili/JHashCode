9adab2c52cfdd5ae05c6346c022b3fff8a7d6389
davide <davide@davide-SVE1512H1EW>
2017-08-01 19:25:18 +0200

    Copyright notice on top of the file

--- src/it/dsestili/jhashcode/core/IScanProgressListener.java
+++ src/it/dsestili/jhashcode/core/IScanProgressListener.java
@@ -1,7 +1,3 @@
-package it.dsestili.jhashcode.core;
-
-import java.util.EventListener;
-
 /*
 JHashCode a simple hash code generator
 Copyright (C) 2013-2016 Davide Sestili
@@ -19,6 +15,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+package it.dsestili.jhashcode.core;
+
+import java.util.EventListener;
 
 public interface IScanProgressListener extends EventListener
 {
